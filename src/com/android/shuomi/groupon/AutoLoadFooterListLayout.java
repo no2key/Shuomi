4008dff78f5bd88193c8289980e1eb353c92cb79
Gary Li <wishyouluck@gmail.com>
2011-10-18 20:26:20 +0800

    add missing NetworkRequestLayout

---
+++ src/com/android/shuomi/groupon/AutoLoadFooterListLayout.java
@@ -0,0 +1,11 @@
+package com.android.shuomi.groupon;
+
+import android.content.Context;
+
+public abstract class AutoLoadFooterListLayout extends ListLayout {
+
+	public AutoLoadFooterListLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/com/android/shuomi/groupon/GrouponDetailsView.java
@@ -0,0 +1,13 @@
+package com.android.shuomi.groupon;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class GrouponDetailsView extends LinearLayout {
+
+	public GrouponDetailsView( Context context ) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
